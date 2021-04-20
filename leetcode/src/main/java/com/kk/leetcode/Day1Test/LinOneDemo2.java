776c276b36c16af44170f567aa1e39c455181f54
kkget <1462018576@qq.com>
2021-04-20 22:51:06 +0800

    leetCode  数组,链表，队列

--- leetcode/src/main/java/com/kk/leetcode/Day1Test/LinOneDemo2.java
+++ leetcode/src/main/java/com/kk/leetcode/Day1Test/LinOneDemo2.java
@@ -28,6 +28,26 @@ public class LinOneDemo2 {
         head.next = null;
         return cur;
     }
+    /**
+     * 迭代方法
+     * 1 -> 2 -> 3 -> 4 -> null
+     * null <- 1 <- 2 <- 3 <- 4
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseListIterative(ListNode head) {
+        ListNode prev = null; //前指针节点
+        ListNode curr = head; //当前指针节点
+        //每次循环，都将当前节点指向它前面的节点，然后当前节点和前节点后移
+        while (curr != null) {
+            ListNode nextTemp = curr.next; //临时节点，暂存当前节点的下一节点，用于后移
+            curr.next = prev; //将当前节点指向它前面的节点
+            prev = curr; //前指针后移
+            curr = nextTemp; //当前指针后移
+        }
+        return prev;
+    }
 
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
